abde57934528cfc7f168a9b0b0005ceb4d1c7a23
gelbesang <ojo1995@naver.com>
2019-07-05 15:50:08 +0900

    이항 연산자
    논리 연산자까지

---
+++ day02-Types/src/opertator/binary/ScoreInput.java
@@ -0,0 +1,50 @@
+package opertator.binary;
+
+import java.util.Scanner;
+
+/**
+ * &&, || : 논리 연산 AND, OR 연산자
+ * ----------------------------------
+ * 반드시 기호를 겹쳐서 쓸것.
+ * 
+ * 입력 된 점수가 0 ~ 100 사이의 값인지
+ * 판단하는 클래스
+ * 
+ * 논리 연산자 &&를 테스트한다.
+ * @author Administrator
+ *
+ */
+public class ScoreInput {
+
+	public static void main(String[] args) {
+		// 1. 선언
+		// 입력값 저장할 변수 선언
+		int score;
+		
+		// 입력처리를 위한 스캐너 선언
+		Scanner scan; // import : crtl + shift o 단추키로 입력
+		
+		// 2. 초기화 
+		// 스캐너 초기화
+		scan = new Scanner(System.in);
+		System.out.println("점수를 입력");
+		// 변수에 스캐너로 값을 입력하여 초기화
+		score = scan.nextInt();
+		
+		// 3. 입력 된 값의 범위를 판단.
+		// 0 <= score <= 100
+		/*
+		 * WHERE score >= 0 
+		 *   AND score <= 100
+		 */
+		if (score >= 0 && score <= 100) {
+			System.out.printf("%d 은(는) 유효한 점수 입니다.%n" , score);
+		}else {
+			System.out.printf("%d 은(는) 벗어난 점수 입니다.%n" , score);
+
+			
+		}
+	
+	}
+
+}
