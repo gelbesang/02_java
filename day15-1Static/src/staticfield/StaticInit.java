dae593bf0c66e3dc2e667db1f7c3aa6c9c5d8b3f
gelbesang <ojo1995@naver.com>
2019-08-02 09:31:27 +0900

    satic 수정

--- day15-1Static/src/staticfield/StaticInit.java
+++ day15-1Static/src/staticfield/StaticInit.java
@@ -1,214 +1,118 @@
-package book;
+package staticfield;
 
 /**
- * Book 여러개가 꼽혀있어
- * 책 객체 여러개를 한번에 저장하고 
- * 관리할 수 있는 클래스
+ * static 멤버(변수, 메소드) 와
+ * non-static 멤버(변수, 메소드) 의
  * 
- * ==================================================
- * 1. 캡슐화 적용 : 멤버변수는 private
- *                  생성자, 메소드는 public
- *                  
- * ==================================================
+ * 초기화 시점에 따른 참조를 구성하는 클래스 
+ * 
  * @author 304
  *
  */
-public class BookShelf {
+public class StaticInit {
+	
+	// 1. 변수선언
+	// (1) 스태틱 변수 선언(클래스 변수 선언)
+	//     static 변수는 생성자가 초기화하는 영역이 아니므로
+	//     선언하면서 필요한 초기값을 바로 할당한다.
+	static int count = 0;
+	
+	// (2) 멤버 변수 선언(인스턴스 변수 선언) 
+	int memberCnt;
+	
+	// 2. 생성자 선언 생략 ==> 기본 생성자 자동 제공
+	
+	// 3. 메소드 선언
+	// (1) 접근자 / 수정자 ==> 멤버 변수에 대해서 제공
+	public int getMemberCnt() {
+		return memberCnt;
+	}
+	
+	public void setMemberCnt(int memberCnt) {
+		this.memberCnt = memberCnt;
+	}
+	
+	// (2) 그 외 기타 기능 메소드
+	/**
+	 * static 필드인 count 변수의 값을 리턴하는 메소드
+	 * static 메소드로 작성
+	 * 
+	 * @return count : int
+	 */
+	public static int getCount() {
+		return count;
+	}
+	
+	/**
+	 * static 필드인 count 변수를 증가시키는 메소드
+	 */
+	public static void increaseCount() {
+		count++;
+	}
+	
+	/**
+	 * static 필드인 count 변수와
+	 * non-static 필드인 memberCnt 변수의 
+	 * 값을 모두 출력하는 메소드
+	 */
+	public static void print() {
+		System.out.println("print1=================");
+		// 1. count 출력
+		System.out.println("count=" + count);
+		
+		// 2. memberCnt 출력
+//		System.out.println("memberCnt=" + memberCnt);
+//		System.out.println("memberCnt=" + getMemberCnt());
+		/* -------------------------------------------
+		 * 윗줄의 오류 이유 : print() 메소드는 static
+		 *                    memberCnt 변수는 non-static
+		 * -------------------------------------------
+		 * static 필드에서 non-static 필드를 참조(사용)
+		 * 하려면 바로 사용 할 수 없고
+		 * 그 non-static 필드를 가지고 있는 클래스의
+		 * 인스턴스를 생성한 후(new .... : 명시적 초기화) 
+		 * 생성한 인스턴스.non-static 필드 이름 으로 접근
+		 * 하는 것만 가능하다.
+		 * -------------------------------------------
+		 */
+		StaticInit init = new StaticInit();
+		System.out.println("memberCnt=" + init.memberCnt);
+		System.out.println("memberCnt=" + init.getMemberCnt());
+	}
+	
+	/**
+	 * 이 클래스의 count 값과
+	 * 입력된 StaticInit 객체로부터 memberCnt 값을 출력
+	 * @param init
+	 */
+	public static void print(StaticInit init) {
+		System.out.println("print2=================");
+		System.out.println("count=" + count);
+		System.out.println("memberCnt=" + init.memberCnt);
+	}
+	
+	/**
+	 * 이 클래스의 클래스 변수(static 변수)인 count와 
+	 * 인스턴스 변수인 memberCnt 를 
+	 * 더한 값을 리턴하는 메소드
+	 * -----------------------------------------------
+	 * 1. 이 메소드는 static modifier 가 붙지 않았으므로
+	 *    non-static 메소드가 된다.
+	 *    
+	 * 2. non-static 메소드인 이 메소드에서
+	 *    static 필드인 count 값을 사용(참조) 하려 할 때
+	 *    아무런 제약없이 바로 사용 가능
+	 *    
+	 * 3. 이유는 static 필드는 초기화 시점이 앞서기 때문
+	 * 
+	 * @return count + memberCnt : int 
+	 */
+	public int addCount() {
+		return this.memberCnt + count;
+	}
+	
 
-	// 멤버변수
-	private Book[] books;
-	
-	public BookShelf() {
-		books = new Book[0];
-	}
-	
-	public BookShelf(Book[] books) {
-		this.books = books;
-	}
-	
-	// 책장에 책을 추가 : add : void : 매개변수로 Book 1개를 받는다.
-	/**
-	 * 1. 리턴을 int 타입으로 변경 추가 성공한 건수를 리턴
-	 * 
-	 * 2. 매개변수로 입력된 book 객체가 
-	 *    이미 목록에 존재하는지 여부를 
-	 * 
-	 *    isExists(book) 메소드를 사용하여 판단하고
-	 *    isExists(book) 의 결과가 false 일 때만
-	 *    현재의 추가로직이 작동하도록 변경
-	 * 
-	 * @param book
-	 * @return 0 : 이미 같인 seq 번호의 책이 있어서
-	 *             추가 실패
-	 *         1 : 새 책 정보 1건이 성공적으로 추가된 경우
-	 */
-	public void add(Book book) {
-		// 현재 books 보다 길이가 1큰 배열을 새로 만든다.
-		// newBooks
-		Book[] newBooks = new Book[books.length + 1];
-		
-		// books 의 모든 책 내용을 새로 생성한 1칸 큰 배열에
-		// 앞쪽부터 복사
-		for (int idx = 0; idx < books.length; idx++) {
-			newBooks[idx] = books[idx];			
-		}
-		
-		// 매개변수로 넘겨진 book 은 마지막 새로 생긴 칸에 저장
-		newBooks[newBooks.length - 1] = book;
-		
-		// 이 클래스의 books 멤버변수에 새로만든 newBooks 저장
-		this.books = newBooks;
-	}
-	
-	// 책장에서 책을 제거 : void : remove(Book book)
-	/**
-	 * 삭제하기 전에 목록에 book 객체가 존재하는지 먼저
-	 * isExists(book) 으로 판단
-	 * 
-	 * 결과가 true 일 때만 삭제로직 진행    1리턴
-	 * 결과가 false 이면 삭제로직 진행 없이 0리턴
-	 * @param book
-	 */
-	public void remove(Book book) {
-		// book 객체의 sequence 가 같으면 같은 책으로 판단해서
-		// 삭제
-		// 폐기 안하고 남는 책을 유지할 새 배열
-		Book[] newBooks;
-		
-		// 1. 폐기할 책이 위치하는 인덱스를 찾기
-		int index = findBookIndex(book);
-		
-		// 2. 폐기할 책의 인덱스가 -1보다 크면
-		//    폐기할 책이 있다는 의미로 판단하고 삭제로직 진입
-		if (index > -1) {
-			// 3. 폐기 안할 책을 유지할 
-			//    새 배열을 지금 배열 크기 - 1 크기로 생성
-			newBooks = new Book[books.length - 1];
-			
-			//  (1) 삭제할 책 앞쪽의 책정보는 같은 인덱스로 복사
-			for (int idx = 0; idx < index; idx++) {
-				newBooks[idx] = books[idx];
-			}
-			
-			//  (2) 삭제할 책 뒤쪽의 남는 책정보는 현재 인덱스 - 1 위치로 복사
-			for (int idx = index; idx < newBooks.length; idx++) {
-				newBooks[idx] = books[idx + 1];
-			}
-			
-			// 6. 남는 책이 복사된 새 배열을
-			//    this.book 에 새로 저장
-			this.books = newBooks;
-		
-		} // end outer if
-		
-	}
-	
-	// 책 정보 수정 : void : set(Book book)
-	/**
-	 * 수정하기 전에 수정할 book 객체가
-	 * 목록에 이미 존재하는지 여부를
-	 * isExists(book) 수행 결과로 판단해서
-	 * 
-	 * true 이면 수정하고 1리턴
-	 * false 이면 수정진행하지 않고 0리턴
-	 * @param book
-	 */
-	public void set(Book book) {
-		// 수정할 book 이 books 배열 
-		// 몇번째 인덱스에 있는지 찾는다.
-		int index = findBookIndex(book);
-		
-		if (index > -1) {
-			books[index] = book;
-		}
-	}
-	
-	// 책 한권 얻기 : Book : get(Book book)
-	public Book get(Book book) {
-		// 입력된 book의 sequence 값과
-		// 내부 배열에 저장된 책들중 일치하는 sequence 를 가진
-		// 책 1권의 정보를 리턴
-		return findBook(book);
-	}
-	
-	// 전체 책 목록을 얻기 : getAllBooks()
-	public Book[] getAllBooks() {
-		return this.books;
-	}
-	
-	// 접근자
-	public Book[] getBooks() {
-		return books;
-	}
-	
-	// 수정자
-	public void setBooks(Book[] books) {
-		this.books = books;
-	}
-
-	// ---------------------------------------
-	/**
-	 * 매개변수 전달된 책 정보와
-	 * 일치하는 일련번호를 가진 책(책 배열: books 에 있는)을
-	 * 찾아서 배열에 안에 들어있는 책을 리턴
-	 * ---------------------------------------
-	 * 같은 책을 찾는 로직을 
-	 * sequence 값 직접 비교에서
-	 * book 객체 비교로 변경
-	 * ---------------------------------------
-	 * @param book
-	 * @return
-	 */
-	private Book findBook(Book book) {
-		Book findBook = null;
-		for (int idx = 0; idx < books.length; idx++) {
-			if (books[idx].getSequence() == book.getSequence()) {
-				// 같은 책 찾았다.
-				findBook = books[idx];
-				break;
-			}
-		}
-		return findBook;
-	}
-	
-	/**
-	 * 같은 책을 찾는 로직을 
-	 * sequence 값 직접 비교에서
-	 * book 객체 비교로 변경
-	 * 
-	 * @param book
-	 * @return
-	 */
-	private int findBookIndex(Book book) {
-		int index = -1;
-		for (int idx = 0; idx < books.length; idx++) {
-			if (books[idx].getSequence() == book.getSequence()) {
-				// 같은 책 찾았다.
-				index = idx;
-				break;
-			}
-		}
-		return index;
-	}
-
-	/**
-	 * 매개변수로 전달된 book(책)이 
-	 * 목록(배열)에 존재하는지 여부를 검색해서
-	 *  
-	 * 존재하면 true
-	 * 존재하지 않으면 false 
-	 * 를 리턴하는 private 메소드
-	 * 
-	 * @param book
-	 * @return true : 찾는 책이 목록에 존재, 
-	 *         false : 찾는 책이 목록에 없을 때
-	 */
-	private boolean isExists(Book book) {
-		
-	}
 }
-
-
 
 
 
