1e245767ba60f08600b847dce3b426d4f6e16bfa
Jin Oh <ojo1995@naver.com>
2019-08-14 02:31:11 +0900

    가위 바위 보 게임 완성

--- myProject/src/rockPaperScissors/Rock_Paper_Scissors.java
+++ myProject/src/rockPaperScissors/Rock_Paper_Scissors.java
@@ -15,12 +15,19 @@ public class Rock_Paper_Scissors {
 		
 		int coin = scan.nextInt();
 		
+		/*
+		 * 카운트에다가 주는것보단
+		   이긴거를 코인횟수로 나눠서 퍼센트로 구하면
+			승률 나올껄요
+
+		 * */
+		int vic = 0;
 		
 		// 3번만 게임 하게 하기
 		for (int i = 0; i < coin; i++) {
 			
 			int player;
-			int count = 0;
+			
 
 			while (true) {
 				int com = (int) (Math.random() * 3) + 1; // 랜덤 표출
@@ -32,35 +39,38 @@ public class Rock_Paper_Scissors {
 					System.out.println("비겼습니다\n");
 					System.out.printf("com=%d\n", com);
 
-					System.out.printf("승리횟수%d :\n", count);
+//					System.out.printf("승리횟수%d :\n", vic);
 					break;
 				} else if (com == 1 && player == 3) {
 					System.out.println("컴퓨터가 이겼습니다.\n");
 					System.out.printf("com=%d\n", com);
-					count--;
-					System.out.printf("승리횟수%d :\n", count);
+					
+//					System.out.printf("승리횟수%d :\n", vic);
 					break;
 				} else if (com == 2 && player == 1) {
 					System.out.println("컴퓨터가 이겼습니다.\n");
 					System.out.printf("com=%d\n", com);
-					count--;
-					System.out.printf("승리횟수%d :\n", count);
+					
+//					System.out.printf("승리횟수%d :\n ", vic);
 					break;
 				} else if (com == 3 && player == 2) {
 					System.out.println("컴퓨터가 이겼습니다.\n");
 					System.out.printf("com=%d\n", com);
-					count--;
-					System.out.printf("승리횟수%d :\n", count);
+					
+//					System.out.printf("승리횟수%d :\n", vic);
 					break;
 				} else {
 					System.out.println("player가 이겼습니다.\n");
 					System.out.printf("com=%d\n", com);
-					count++;
-					System.out.printf("승리횟수%d :\n", count);
+					vic++;
+					
 					break;
 				}
-
 			}
 		}
+//		System.out.printf("승리횟수%d :\n", vic);
+//		System.out.printf("승률은%d%% :\n", coin/vic*100);
+		double winning = ((double)vic/(double)(coin))*100;
+		System.out.printf("[승률] %.2f%% \n",winning);
 	}
 }
