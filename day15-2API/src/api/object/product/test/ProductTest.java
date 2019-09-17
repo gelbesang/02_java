e488edb92c384587d81f53fb7b41d567caa194f5
Jin Oh <ojo1995@naver.com>
2019-09-18 03:16:46 +0900

    api String

--- day15-2API/src/api/object/product/test/ProductTest.java
+++ day15-2API/src/api/object/product/test/ProductTest.java
@@ -14,13 +14,14 @@ public class ProductTest {
 		
 		// 2. 초기화
 		msMouse = new Product("P001", "MS-아크 터치 마우스", 51330, 36);
-		ltMouse = new Product("P003", "로지텍코리아 G302 Daedalus Prime", 34900, 12);
+		ltMouse = new Product("P003", "로지텍코리아 G302 Daedalus Prime"
+				, 34900, 12);
 		
 		airFrier = new Product();
 		airFrier.setPcode("P002");
 		airFrier.setPname("[필립스] 에어프라이어 HD-9743");
 		airFrier.setPrice(329000);
-		airFrier.setquantity(5);
+		airFrier.setQuantity(5);
 		
 		// products 배열에 원소 할당
 		products[0] = msMouse;
@@ -35,21 +36,19 @@ public class ProductTest {
 		
 		// (2) ms마우스의 가격을 변경
 		Product msMouse2 = new Product
-				("P001", "MS-아크 터치 마우스", 40000, 36);
+				(new String("P001"), "MS-아크 터치 마우스", 40000, 36);
 		
 		// 제품들이 들어있는 배열 products 에서
 		// msMouse2 의 pcode 와 같은 제품번호를 갖는 
 		// 인덱스의 원소를 msMouse2 로 변경
 		for (int idx = 0; idx < products.length; idx++) {
-			// 객체의 내용 비교를 하려면 .equalse()를 사용할것
+			// 객체의 내용비교를 하려면 .equals() 를 사용할 것
 //			if (products[idx].getPcode() == msMouse2.getPcode()) {
-			if(products[idx].equals(msMouse)) {
-			   products[idx] = msMouse2;
+//			if (products[idx].getPcode().equals(msMouse2.getPcode())) {
+			if (products[idx].equals(msMouse2)) {
+				products[idx] = msMouse2;
 			}
 		}
 	}
 
 }
-
-
-
